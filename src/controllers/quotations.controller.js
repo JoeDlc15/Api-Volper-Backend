e1b414658b86847025bec109aa498f8569b0de78
JoeDlc15 <Limache2306@outlook.com>
2026-06-20 17:38:46 -0500

    Corregir dependencias y auth en reviewTempQuotation

--- src/controllers/quotations.controller.js
+++ src/controllers/quotations.controller.js
@@ -937,7 +937,36 @@ exports.reviewTempQuotation = async (req, res) => {
             return res.status(400).json({ error: "Faltan configurar las credenciales de Ventas/Administrador." });
         }
 
-        const { login, client } = require('../utils/auth');
+        const axios = require('axios');
+        const { wrapper } = require('axios-cookiejar-support');
+        const { CookieJar } = require('tough-cookie');
+        const cheerio = require('cheerio');
+
+        const jar = new CookieJar();
+        const client = wrapper(axios.create({
+            jar,
+            withCredentials: true,
+            baseURL: 'https://volperseal.goldensystem.com.pe'
+        }));
+
+        async function login(email, password) {
+            const loginPage = await client.get('/login');
+            const $ = cheerio.load(loginPage.data);
+            const csrfToken = $('input[name="_token"]').val();
+            if (!csrfToken) throw new Error("No se pudo extraer el Token CSRF.");
+
+            const loginResponse = await client.post('/login', new URLSearchParams({
+                '_token': csrfToken,
+                'email': email,
+                'password': password
+            }));
+
+            const finalUrl = loginResponse.request.res ? loginResponse.request.res.responseUrl : loginResponse.request.path;
+            if (finalUrl && finalUrl.endsWith('/login')) {
+                 throw new Error("Credenciales inválidas");
+            }
+        }
+
         await login(email, password);
 
         const quotId = number.replace(/\D/g, '');
